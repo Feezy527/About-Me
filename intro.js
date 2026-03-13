2611017d2f7e6ebc3ebe0d32c4f8228a8aaccbb3
Feezy <afeezalamu527@gmail.com>
2026-03-12 20:48:05 -0900

    Initial commit: Introduction Project

---
+++ intro.js
@@ -0,0 +1,4 @@
+function data(name, age, favProg) {
+    return "My name is " + name + ". I am " + age + " years old and my favourite Progaming Language is " + favProg + ".";
+}
+console.log(data("Afeez", 22, "Javascript"));
