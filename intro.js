af2ec9adfd7e71e56ba73ac2a4a77f1ac404ff51
Feezy <afeezalamu527@gmail.com>
2026-03-14 17:57:41 -0900

    Updated my project

--- intro.js
+++ intro.js
@@ -1,4 +1,5 @@
 function data(name, age, favProg) {
-    return "My name is " + name + ". I am " + age + " years old and my favourite Progaming Language is " + favProg + ".";
+    console.log("My name is " + name + ". I am " + age + " years old and my favourite Progaming Language is " + favProg + ".");
 }
-console.log(data("Afeez", 22, "Javascript"));
+
+data("Afeez", 22, "Javascript");
