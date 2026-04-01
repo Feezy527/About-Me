01290d6a7d3fe4c36131d206864cbf1e31159ce9
Feezy <afeezalamu527@gmail.com>
2026-04-01 13:45:40 -0900

    express

--- backend-course-week-2/server.js
+++ backend-course-week-2/server.js
@@ -6,42 +6,34 @@ const app = express();
 const PORT = process.env.PORT;
 
 
-// Middleware
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send("Something went wrong!");
-    console.log(`${req.method} ${req.url} - ${new Date()}`)
-    next();
-});
 
-//Routes
+
 app.get("/", (req, res) => {
-    res.send("My Week 2 API")
     res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
-
 app.post("/user", (req, res) => {
     const {name, email} = req.body;
-
+    
     if (!name || !email){
         return res.status(400).json({error : "Name and Email is required"})
     }
     res.status(201).json({message: `Hello ${name}!`});
 });
 
-
 app.get("/user/:id", (req, res)=> {
     const {id} = req.params; 
-    res.send(`User = ${id}`);
+    res.send(`User ${id} profile`);
 });
 
-
-
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
+});
 
 app.listen(PORT, ()=> {
     console.log(`Running on http://localhost:${ PORT }`);
